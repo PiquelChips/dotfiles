50232a826ba195d3cb0115c1626cb0ee33cc78bc
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-03-21 18:07:32 +0100

    fix: changed git conf to use ssh instead of https

--- nix/users/piquel/terminal/cli.nix
+++ nix/users/piquel/terminal/cli.nix
@@ -11,6 +11,11 @@
                     name = "PiquelChips";
                     email = "63727792+PiquelChips@users.noreply.github.com";
                 };
+                url = {
+                    "git@github.com:" = {
+                        insteadOf = [ "https://github.com/" ];
+                    };
+                };
                 filter."lfs" = {
                     clean = "git-lfs clean -- %f";
                     smudge = "git-lfs smudge -- %f";
