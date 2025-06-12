8da7df20ed9a00f2e03997318a2551fdc4e14e9b
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-06-12 16:59:55 +0200

    add validation layers

--- nix/users/piquel/piquel.nix
+++ nix/users/piquel/piquel.nix
@@ -42,6 +42,7 @@
                 vulkan-tools
                 shaderc
                 spirv-tools
+                vulkan-validation-layers
             ];
         };
     };
@@ -90,11 +91,12 @@
             LANG="en_US.UTF-8";
             EDITOR="nvim";
 
-            JDTLS_JVM_ARGS = "-javaagent:${pkgs.lombok}/share/java/lombok.jar";
             PKG_CONFIG_PATH = "/run/current-system/sw/lib/pkgconfig:${pkgs.wayland.dev}/lib/pkgconfig:${pkgs.libffi.dev}/lib/pkgconfig:${pkgs.libxkbcommon.dev}/lib/pkgconfig:${pkgs.vulkan-loader.dev}/lib/pkgconfig:${pkgs.SDL2.dev}/lib/pkgconfig";
             DIRK_ENGINE_CMAKE_ARGS = "-DGLFW_BUILD_X11=OFF -DBUILD_WSI_XCB_SUPPORT=OFF -DBUILD_WSI_XLIB_SUPPORT=OFF";
-
+            VK_LAYER_PATH = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
             LD_LIBRARY_PATH="${pkgs.wayland}/lib:${pkgs.libxkbcommon}/lib";
+
+            JDTLS_JVM_ARGS = "-javaagent:${pkgs.lombok}/share/java/lombok.jar";
         };
     };
 }
