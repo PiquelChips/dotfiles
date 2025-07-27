a2aaa4e696b961cbe28f29bb0988dc0e5eb58d1a
PiquelChips <63727792+PiquelChips@users.noreply.github.com>
2025-07-27 18:25:52 +0200

    update config

--- nix/users/piquel/terminal/cli.nix
+++ nix/users/piquel/terminal/cli.nix
@@ -22,7 +22,6 @@
                     process = "git-lfs filter-process";
                     required = true;
                 };
-                push.autoSetupRemote = true;
             };
         };
         neovim = {
