75904c4eb1f37edba9accb4fa2238877b9b4616b
PiquelChips <piquel@piquel.fr>
2026-01-31 17:42:16 +0100

    remake flake

--- flake.nix
+++ flake.nix
@@ -7,15 +7,33 @@
             url = "github:PiquelChips/piquel-cli";
             inputs.nixpkgs.follows = "nixpkgs";
         };
+        nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     };
 
-    outputs = { nixpkgs, ... } @ inputs: {
-        nixosConfigurations."piquel" = nixpkgs.lib.nixosSystem {
-            specialArgs = { inherit inputs; };
-            modules = [
-                ./nix/users/piquel/piquel.nix
-                ./nix/system.nix
-            ];
+    outputs = { nixpkgs, ... } @ inputs: 
+    let
+        systems = [
+            "x86_64-linux"
+            "aarch64-linux"
+            "aarch64-darwin"
+        ];
+
+        forAllSystems = nixpkgs.lib.genAttrs systems;
+    in {
+        packages = forAllSystems (system: import ./nix/pkgs nixpkgs.legacyPackages.${system});
+
+        formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt);
+
+        overlays = import ./nix/overlays {inherit inputs;};
+        nixosModules = import ./nix/modules;
+
+        nixosConfigurations = {
+            piquel = nixpkgs.lib.nixosSystem {
+                specialArgs = { inherit inputs; };
+                modules = [
+                    ./nix/piquel.nix
+                ];
+            };
         };
     };
 }
