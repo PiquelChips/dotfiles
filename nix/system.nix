4785565bab80a9756c041d9f487283f33d8b494a
PiquelChips <piquel@piquel.fr>
2026-01-31 17:20:32 +0100

    update trusted users

--- nix/system.nix
+++ nix/system.nix
@@ -95,7 +95,7 @@
         settings = {
             auto-optimise-store = true;
             experimental-features = [ "nix-command" "flakes" ];
-            trusted-users = [ "piquel" ];
+            trusted-users = [ "root" "@wheel" ];
         };
     };
 }
