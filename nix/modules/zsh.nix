f6982376aa3246a417e8708fdd4c1ac6b2b7f809
PiquelChips <piquel@piquel.fr>
2026-02-05 17:16:18 +0100

    update zsh prompt

--- nix/modules/zsh.nix
+++ nix/modules/zsh.nix
@@ -49,8 +49,14 @@ in
             promptInit = ''
                 CASE_SENSITIVE="true"
                 
-                PROMPT="%(?:%{$fg_bold[green]%}%1{➜%} :%{$fg_bold[red]%}%1{➜%} ) %{$fg[cyan]%}%~%{$reset_color%}"
-                PROMPT+=' $(git_prompt_info)'
+                PROMPT="%(?:%{$fg_bold[green]%}%1{➜%} :%{$fg_bold[red]%}%1{➜%} )%{$fg[cyan]%}"
+
+                if [[ -n "$SSH_CONNECTION" ]] || [[ -n "$SSH_CLIENT" ]] || [[ -n "$SSH_TTY" ]]; then
+                    PROMPT+="%n@%m:"
+                fi
+
+                PROMPT+="%~%{$reset_color%}"
+                PROMPT+=" $(git_prompt_info)"
                 
                 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}git:(%{$fg[red]%}"
                 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
