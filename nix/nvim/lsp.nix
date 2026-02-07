7a2f1170165ac9428e7d90ca85ae73e2a2e6244c
PiquelChips <piquel@piquel.fr>
2026-02-07 12:38:31 +0100

    setup lsp keymaps

--- nix/nvim/lsp.nix
+++ nix/nvim/lsp.nix
@@ -1,3 +1,12 @@
-{ ... }:
+{ lib, ... }:
 {
+    lsp = {
+        keymaps = [
+            { key = "gd"; lspBufAction = "definition"; }
+            { key = "gr"; action = lib.nixvim.mkRaw "require('telescope.builtin').lsp_references"; }
+            { key = "gt"; lspBufAction = "type_definition"; }
+            { key = "gi"; lspBufAction = "implementation"; }
+            { key = "K"; lspBufAction = "hover"; }
+        ];
+    };
 }
