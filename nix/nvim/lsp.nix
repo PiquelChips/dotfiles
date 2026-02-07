03e7fdc7c98b28a943cc6303522d3504b85144cc
PiquelChips <piquel@piquel.fr>
2026-02-07 13:20:23 +0100

    setup base lsp config

--- nix/nvim/lsp.nix
+++ nix/nvim/lsp.nix
@@ -1,6 +1,47 @@
 { lib, ... }:
 {
     lsp = {
+        inlayHints.enable = true;
+        servers = {
+            "*" = {
+                enable = true;
+                config = {
+                #     capabilities = {
+                #         textDocument = {
+                #             completion = {
+                #                 completionItem = {
+                #                     commitCharactersSupport = true;
+                #                     deprecatedSupport = true;
+                #                     insertReplaceSupport = true;
+                #                     insertTextModeSupport = {
+                #                         valueSet = [ 1 2 ];
+                #                     };
+                #                     labelDetailsSupport = true;
+                #                     preselectSupport = true;
+                #                     resolveSupport = {
+                #                         properties = [ "documentation" "additionalTextEdits" "insertTextFormat" "insertTextMode" "command" ];
+                #                     };
+                #                     snippetSupport = true;
+                #                     tagSupport = {
+                #                         valueSet = [ 1 ];
+                #                     };
+                #                 };
+                #                 completionList = {
+                #                     itemDefaults = [ "commitCharacters" "editRange" "insertTextFormat" "insertTextMode" "data" ];
+                #                 };
+                #                 contextSupport = true;
+                #                 dynamicRegistration = false;
+                #                 insertTextMode = 1;
+                #             };
+                #         };
+                #     };
+                };
+                root_markers = [
+                    ".git"
+                ];
+            };
+        };
+
         keymaps = [
             { key = "gd"; mode = [ "n" ]; lspBufAction = "definition"; }
             { key = "gr"; mode = [ "n" ]; action = lib.nixvim.mkRaw "require('telescope.builtin').lsp_references"; }
