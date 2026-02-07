3abea6a5ca21c8dd2e14960901e50b50248b0819
PiquelChips <piquel@piquel.fr>
2026-02-07 14:23:34 +0100

    cleanup config

--- nix/nvim/lsp.nix
+++ nix/nvim/lsp.nix
@@ -3,43 +3,6 @@
     lsp = {
         inlayHints.enable = false;
         servers = {
-            "*" = {
-                enable = true;
-                config = {
-                #     capabilities = {
-                #         textDocument = {
-                #             completion = {
-                #                 completionItem = {
-                #                     commitCharactersSupport = true;
-                #                     deprecatedSupport = true;
-                #                     insertReplaceSupport = true;
-                #                     insertTextModeSupport = {
-                #                         valueSet = [ 1 2 ];
-                #                     };
-                #                     labelDetailsSupport = true;
-                #                     preselectSupport = true;
-                #                     resolveSupport = {
-                #                         properties = [ "documentation" "additionalTextEdits" "insertTextFormat" "insertTextMode" "command" ];
-                #                     };
-                #                     snippetSupport = true;
-                #                     tagSupport = {
-                #                         valueSet = [ 1 ];
-                #                     };
-                #                 };
-                #                 completionList = {
-                #                     itemDefaults = [ "commitCharacters" "editRange" "insertTextFormat" "insertTextMode" "data" ];
-                #                 };
-                #                 contextSupport = true;
-                #                 dynamicRegistration = false;
-                #                 insertTextMode = 1;
-                #             };
-                #         };
-                #     };
-                    root_markers = [
-                        ".git"
-                    ];
-                };
-            };
 
             gopls.enable = true;
             bashls.enable = true;
@@ -58,14 +21,7 @@
 
             clangd = {
                 enable = true;
-                config = {
-                    cmd = [
-                        "clangd"
-                            "--clang-tidy"
-                            "--background-index"
-                            "--query-driver=/etc/profiles/per-user/piquel/bin/g++,/etc/profiles/per-user/piquel/bin/gcc"
-                    ];
-                };
+                config.cmd = [ "clangd" "--clang-tidy" "--background-index" ];
             };
 
             denols = {
