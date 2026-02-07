7006b00aac67f8f160dfda97aaae63f2c2f5d51d
PiquelChips <piquel@piquel.fr>
2026-02-07 18:26:15 +0100

    clean

--- nix/configs/common.nix
+++ nix/configs/common.nix
@@ -17,14 +17,8 @@
         };
         settings = {
             auto-optimise-store = true;
-            experimental-features = [
-                "nix-command"
-                "flakes"
-            ];
-            trusted-users = [
-                "root"
-                "@wheel"
-            ];
+            experimental-features = [ "nix-command" "flakes" ];
+            trusted-users = [ "root" "@wheel" ];
         };
     };
 
