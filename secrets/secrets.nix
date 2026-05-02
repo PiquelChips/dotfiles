bff4caf578d8aa2237558065b9698a433905f942
piquel <piquel@piquel.fr>
2026-05-02 16:34:50 +0200

    add secrets.nix

---
+++ secrets/secrets.nix
@@ -0,0 +1,14 @@
+let
+  # Piquel's personal public key
+  piquel = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHVqRluVYJXXoNYyFQzkZm2v2bRnAv/PNuoLRr2G2/Dv";
+  users = [ piquel ];
+
+  nixosbtw = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL2JWt8eVq7rh8Iv41c78xhW3MGKn5m7MBNQ/3iLUWM8";
+  piquel-pi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKtrAhMMQ4WDysvu1zbxrnARtl+B0TwR1ov5MC8Wut0F";
+  systems = [
+    nixosbtw
+    piquel-pi
+  ];
+in
+{
+}
