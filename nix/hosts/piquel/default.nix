66c87ad49c49e6f93d859676e1a7f428ce83bc53
piquel <piquel@piquel.fr>
2026-05-25 10:42:04 +0200

    add gh cli

--- nix/hosts/piquel/default.nix
+++ nix/hosts/piquel/default.nix
@@ -51,7 +51,7 @@
               typescript-language-server
               vscode-langservers-extracted
               # Utils
-              feh wl-clipboard stow tree yazi lazydocker
+              gh feh wl-clipboard stow tree yazi lazydocker
               ffmpeg fd ripgrep imagemagick poppler fzf air
               sqlc docker-buildx neofetch zoxide gnumake mpv
               p7zip postgresql cmake pkg-config tailwindcss_4
