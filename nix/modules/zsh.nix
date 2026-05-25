c4bdce743f52c5e01fbe3007779e67d68ad3cee7
piquel <piquel@piquel.fr>
2026-05-25 10:18:44 +0200

    update PATH to have npm global

--- nix/modules/zsh.nix
+++ nix/modules/zsh.nix
@@ -37,7 +37,7 @@
         shellInit = ''
           fpath=(~/dotfiles/config/zsh-completions $fpath)
 
-          export PATH="$HOME/go/bin:$HOME/.cargo/bin:$PATH"
+          export PATH="$HOME/go/bin:$HOME/.cargo/bin:$HOME/.npm-global/bin:$PATH"
           export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$NIX_LD_LIBRARY_PATH"
 
           source ${fzf-tab}/fzf-tab.plugin.zsh
