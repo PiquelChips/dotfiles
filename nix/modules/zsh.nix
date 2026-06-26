ca3aa8052afef16c74c495b2a469a1a6278aeae6
piquel <piquel@piquel.fr>
2026-06-26 18:53:45 +0200

    cleanup shell

--- nix/modules/zsh.nix
+++ nix/modules/zsh.nix
@@ -26,7 +26,7 @@
 
         shellAliases = {
           l = "ls -alF";
-          p = "piquel load $(piquel list | fzf)";
+          p = "piquel pick";
         };
 
         ohMyZsh = {
@@ -37,7 +37,9 @@
         shellInit = ''
           fpath=(~/dotfiles/config/zsh-completions $fpath)
 
-          export PATH="$HOME/go/bin:$HOME/.cargo/bin:$PATH"
+          export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$NIX_LD_LIBRARY_PATH"
+          path=("$HOME/go/bin" "$HOME/.cargo/bin" "$HOME/.npm-global/bin" "$HOME/.local/share/pnpm/bin" $path)
+          typeset -U path
 
           source ${fzf-tab}/fzf-tab.plugin.zsh
         '';
