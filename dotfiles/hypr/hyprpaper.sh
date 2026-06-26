33a14f92ac69342e64b809a3ff269cf880a7006f
piquel <piquel@piquel.fr>
2026-06-26 18:39:10 +0200

    update hyprpaper to use new API

--- dotfiles/hypr/hyprpaper.sh
+++ dotfiles/hypr/hyprpaper.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 hyprdir="$HOME/.config/hypr"
+hyprconf="$hyprdir/hyprpaper.conf"
+
 wallpaper_dir="$hyprdir/wallpaper"
 files=($wallpaper_dir/*)
 
 rm $hyprdir/hyprpaper.conf
 
+
 set_wallpaper() {
 
     random_file=("${files[RANDOM % ${#files[@]}]}")
-    echo "preload=$random_file" >> $hyprdir/hyprpaper.conf
-    echo "wallpaper=$1,$random_file" >> $hyprdir/hyprpaper.conf
+    echo "wallpaper {" >> "$hyprconf"
+    echo "    monitor = $1" >> "$hyprconf"
+    echo "    path = $random_file" >> "$hyprconf"
+    echo "    fit_mode = cover" >> "$hyprconf"
+    echo "}" >> "$hyprconf"
+    echo "" >> "$hyprconf"
+
 }
 
 set_wallpaper "DP-3"
