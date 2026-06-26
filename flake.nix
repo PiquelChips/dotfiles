ef04148de93b97b3ec89915bd3e0d729e9b24d37
piquel <piquel@piquel.fr>
2026-06-26 18:57:08 +0200

    add systems for systems

--- flake.nix
+++ flake.nix
@@ -5,11 +5,13 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
+    systems.url = "github:nix-systems/default";
 
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-parts.follows = "flake-parts";
+      inputs.systems.follows = "systems";
     };
 
     fenix = {
@@ -31,11 +33,7 @@
   outputs =
     inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
+      systems = import inputs.systems;
       imports = [
         ./nix
         ./nix/hosts
