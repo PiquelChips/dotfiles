44e3129494eada171e9bd33263bb883affe0860e
piquel <piquel@piquel.fr>
2026-06-26 18:49:25 +0200

    move to nixos unstable

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "Piquel system configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
 
     nixvim = {
-      url = "github:nix-community/nixvim/nixos-25.11";
+      url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-parts.follows = "flake-parts";
     };
