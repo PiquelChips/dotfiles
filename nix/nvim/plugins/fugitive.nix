dd423f0a60f4cfaea18f514236e64e4f52b0fd0c
piquel <piquel@piquel.fr>
2026-07-06 21:02:33 +0200

    fix fugitive expand dir on MacOS

--- nix/nvim/plugins/fugitive.nix
+++ nix/nvim/plugins/fugitive.nix
@@ -1,15 +1,28 @@
-{ ... }:
+{ lib, pkgs, ... }:
 {
-    plugins.fugitive.enable = true;
+  plugins.fugitive.enable = true;
 
-    keymaps = [
-        {
-            action = "<cmd>Git<cr>";
-            key = "<leader>gs";
-            mode = [ "n" ];
-            options = {
-                desc = "Git status";
-            };
-        }
-    ];
+  keymaps = [
+    {
+      action = "<cmd>Git<cr>";
+      key = "<leader>gs";
+      mode = [ "n" ];
+      options = {
+        desc = "Git status";
+      };
+    }
+  ];
+
+  extraConfigLua = lib.optionalString pkgs.stdenv.isDarwin ''
+    vim.api.nvim_create_autocmd("FileType", {
+        pattern = "fugitive",
+        callback = function(event)
+            vim.keymap.set("n", "-", "=", {
+                buffer = event.buf,
+                remap = true,
+                desc = "Fugitive expand diff",
+            })
+        end,
+    })
+  '';
 }
