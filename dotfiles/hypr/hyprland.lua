d15c0e0cd497de1444a82bca901b457bf057b9da
piquel <piquel@piquel.fr>
2026-08-29 16:27:19 +0200

    swap super & alt on nixos

--- dotfiles/hypr/hyprland.lua
+++ dotfiles/hypr/hyprland.lua
@@ -138,7 +138,7 @@ hl.config({
         kb_layout     = "fr",
         kb_variant    = "",
         kb_model      = "",
-        kb_options    = "ctrl:nocaps",
+        kb_options    = "ctrl:nocaps,altwin:swap_alt_win",
         kb_rules      = "",
 
         follow_mouse  = 1,
