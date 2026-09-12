2d6a064a9aee08da9f3ef3ff0b587f89bf669ed3
piquel <piquel@piquel.fr>
2026-09-12 14:39:16 +0200

    avoid using SSH origins

--- nix/modules/common-host.nix
+++ nix/modules/common-host.nix
@@ -133,6 +133,7 @@
         name = "piquel";
         email = "piquel@piquel.fr";
       };
+      url."git@github.com:".insteadOf = "https://github.com/";
       filter."lfs" = {
         clean = "git-lfs clean -- %f";
         smudge = "git-lfs smudge -- %f";
